7f51840909357495bdd5b37405830771c22ec529
millerrowan <144379880+millerrowan@users.noreply.github.com>
2024-03-18 11:50:27 -0700

    commit 1

---
+++ node.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std; 
+
+class node {
+    public: 
+    char data[]; 
+    node* nextNode; 
+    node(char value) {
+     nextNode = nullptr;    
+     data = value;    
+    }
+    
+    
+    
+};
+
+
+    
+    
+    
+    
+    
